e50769ffae1111a4c1cfd4b5d5b9366d9cacf4c5
younishamrh10 <yoha06001@utb.vaxjo.se>
2024-02-16 11:12:38 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inlämningsuppgift_list_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> namnlista = new List<string>();
+
+            Console.WriteLine("Du ska få skriva in namn, avsluta med tom rad");
+            string nynamn = " ";
+            while (nynamn != "")
+            {
+                Console.WriteLine("Skriv in ett nytt namn");
+                nynamn = Console.ReadLine();
+                if(nynamn != "")
+                {
+                    namnlista.Add(nynamn);
+                }
+            }
+
+            Console.WriteLine("Här är det namnen du skriv");
+            namnlista.Sort();
+            namnlista.Reverse();
+
+            foreach (string namn in namnlista)
+            {
+                Console.WriteLine(namn);
+            }
+        }
+    }
+}
